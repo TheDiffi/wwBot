f08d88c0938a5204faa07c73ccf4fe7a46629d88
TheDiffi <TheDiffi@users.noreply.github.com>
2020-05-06 23:49:04 +0200

    cleaned up old code, added Commands join and leave

--- src/main/java/wwBot/Main.java
+++ src/main/java/wwBot/Main.java
@@ -1,7 +1,10 @@
 package wwBot;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
@@ -9,45 +12,93 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 
 public class Main {
 
-    private static final Map<String, Command> commands = new HashMap<>();
+    public static List<String> listPlayers = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
 
+       
 
-        // Commands with the prefix !
-        commands.put("ping", event -> {
-            event.getMessage().getChannel().block().createMessage("Pong!").block();
 
-        });
 
+
+
+
+
+        // speichert den Prefix in einer Variable
+        var prefix = "&";
 
         DiscordClient client = DiscordClientBuilder
-                .create("NzAyOTk2NzAwODIxODQ4MTE0.XqMWkg.tNGZ3Yhq9dyAQDumpZf5dhT9sBo").build();
+                .create("NzA3NjUzNTk1NjQxOTM4MDMx.XrL8Cw.SRxT4UisfP6doLQoc-ZdgI5CtYY").build();
+
+        // looks at every message
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .filter(message -> message.getMessage().getAuthor().map(user -> !user.isBot()).orElse(false))
+                .subscribe(event -> {
+                    // messageContent speichert den Inhalt der message in einer Variable
+                    // command teilt diesen Inhalt bei einem Leerzeichen und speicher dies in einer
+                    // Liste
+                    String messageContent = event.getMessage().getContent().orElse("");
+                    List<String> command = Arrays.asList(messageContent.split(" "));
+
+                    // Schaut ob der erste Teil der Nachricht das Prefix ist
+                    if (messageContent.startsWith(prefix)) {
+
+                        var user = event.getMessage().getAuthor().get().getUsername();
+
+                        // Liste der Commands, content und sonstige parameter werden überprüft
+                        // TODO: prefix aus parameter entfetnen sonst wird 2-mal prefix überprüft =
+                        // schlampig
+
+                        // ping testet ob der bot antwortet
+                        if (command.get(0).equalsIgnoreCase(prefix + "ping")) {
+                            event.getMessage().getChannel().block().createMessage("Pong!").block();
+                        }
+
+                        // join füght den user zu listPlayers hinzu
+                        if (command.get(0).equalsIgnoreCase(prefix + "join")) {
+
+                            if (listPlayers.indexOf(user) == -1) {
+                                listPlayers.add(user);
+                                event.getMessage().getChannel().block().createMessage("joined").block();
+                            } else {
+                                event.getMessage().getChannel().block().createMessage("looks like you're already joined").block();
+                            }
+                        }
+
+                        // leave entfernt den user von listPlayers
+                        if (command.get(0).equalsIgnoreCase(prefix + "leave")) {
+
+                            if (listPlayers.indexOf(user) != -1) {
+                                listPlayers.remove(user);
+                                event.getMessage().getChannel().block().createMessage("you left").block();
+                            } else {
+                                event.getMessage().getChannel().block().createMessage("looks like you're already not in the game").block();
+                            }
+                        }
 
 
+                    //TODO: füge command startgame hinzu
+                    /*startgame nimmt die .size der listPlayers und started damit den Teambuilder algorithmus
+                    startgame überprüft ob listCustomTeam gleich lang wie listPlayer ist; aka ob jeder Player genau eine Karte hat
+                     */
 
-        client.getEventDispatcher().on(MessageCreateEvent.class).subscribe(event -> {
-            final String content = event.getMessage().getContent().orElse("");
-            for (final Map.Entry<String, Command> entry : commands.entrySet()) {
-                // We will be using ! as our "prefix" to any command in the system.
-                if (content.startsWith('?' + entry.getKey())) {
-                    try {
-                        entry.getValue().execute(event);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                        break;
+                    //TODO: füge command addCard hinzu
+                    
+                    /* addCard lässt jeden Player eine Karte aus den verfügbaren Karten eine Karte zur listCustomTeam hinzufügen
+                    addCard überprüft bei jedem Aufruf ob listCustomTeam nicht größer als listPlayers ist
+                    */
+
+                    //TODO: füge command removeCard hinzu
+                    //removeCard entfernt die Karte aus listCustomTeam
+
+                
+
+                    //TODO: 
+                    
+
                 }
-            }
-        });
-
-
-        /* client.getEventDispatcher().on(MessageCreateEvent.class)
-            .filter(message -> message.getMessage().getAuthor().map(user -> !user.isBot()).orElse(false))
-            .subscribe(event -> {
-
-                reactMessage(event);
-        }); */
+                
+        }); 
         
     
         client.login().block();
@@ -55,15 +106,8 @@ public class Main {
     }
 
 
-    /* public static void reactMessage(MessageCreateEvent event){
-
-        String messageContent = event.getMessage().getContent().orElse("");
-
-        if(messageContent.equalsIgnoreCase("!ping")){
-            event.getMessage().getChannel().block().createMessage("Pong!").block();
-        }
-
-    } */
+    
+    } 
     
     
     
@@ -71,16 +115,6 @@ public class Main {
 
 
 
-
-
-}
- 
-
-
-
-interface Command {
-    void execute(MessageCreateEvent event) throws Exception;
-}
 
 
 
