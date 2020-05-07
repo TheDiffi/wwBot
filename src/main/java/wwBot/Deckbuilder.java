3a64088348d11b0dea344ec5fa389e45e3b5efc5
Nukecrafter <federikko81@gmail.com>
2020-05-07 23:42:56 +0200

    Deck ratios and cleanup

--- src/main/java/wwBot/Deckbuilder.java
+++ src/main/java/wwBot/Deckbuilder.java
@@ -1,28 +1,100 @@
-/*   package wwBot;
+package wwBot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Deckbuilder {
 
-    
-
     public static List<Card> create(int playerAmount) {
-        var availableCards = globals;
-        int totalCardValue;
-        var deckList = new ArrayList<Card>();
+        var availableCards = Globals.mapAvailableCards;
+        var totalValue = 0;
+        var listDeck = new ArrayList<Card>();
+
+        // add Seher
+        int numbSeher = playerAmount > 17 ? 2 : 1;
+        addMultiple(numbSeher, availableCards.get("Seher"), listDeck);
+        availableCards.get("Seher").unique = false;
+
+        // add Werwölfe
+        int numbWerwölfe = (int) Math.pow(((playerAmount - 2) / 3), 0.85);
+        addMultiple(numbWerwölfe, availableCards.get("Werwolf"), listDeck);
+
+        // add Dorfbewohner
+        int numbDorfbewohner = (int) Math.pow(playerAmount * 1.3, 0.65);
+        addMultiple(numbDorfbewohner, availableCards.get("Dorfbewohner"), listDeck);
+
+        // add Spezialkarten
+        int numbSpezialkarten = playerAmount - (numbDorfbewohner + numbWerwölfe + numbSeher);
+        int numbAdjustCards = (int) Math.ceil(numbSpezialkarten/3);
+
+        // add random specialcards außer so viele wie "Ajustierende Karten" sind und setzt den unique Wert der hinzugefügten Karte auf false
+       
+        for (int i = 0; i < (numbSpezialkarten - numbAdjustCards); i++) {
+            var randomCard = getRandomSpezialkarte(numbSpezialkarten, availableCards);
+            listDeck.add(randomCard);
+            availableCards.get(randomCard.name).unique = false;
+        }
+       
+        
+        // add last special-card, with considering card value of all cards
+        for (var card : listDeck) {
+            totalValue += card.value;
+        }
+
+        //wird so oft ausgeführt wie "Ajustierende Karten" sind
+        for (int i = 0; i < numbAdjustCards; i++) {
+            //kalkuliert die summe jeder Karte. Falls die Summe der Karte kleiner ist als die Summe der bisherigen wird sie in mallestDifferenceCard gespeichert. 
+            Card smallestDifferenceCard = null;
+            for (var card : availableCards.values()) {
+                if (card.unique) {
+
+                    if (smallestDifferenceCard == null) {
+                        smallestDifferenceCard = card;
+                    } else if (Math.abs(totalValue + card.value) < Math.abs(totalValue + smallestDifferenceCard.value)) {
+                        smallestDifferenceCard = card;
+                    }
+                }
+            }
+
+            //nachdem alle Karten überprüft wurden wird die Karte, welche Total Value am nächsten zu 0 bringt, zum Deck hinzugefügt
+            listDeck.add(smallestDifferenceCard);
+            availableCards.get(smallestDifferenceCard.name).unique = false;
+    }
+
         
 
-        // add 1 Seher pro deck
-        deckList.add();
+        return listDeck;
+    }
 
-        // add werwölfe
-        int numbWerwölfe = (playerAmount - 2) / 3;
+    public static void addMultiple(final int amount, final Card card, final List<Card> list) {
+        for (int i = 0; i < amount; i++) {
+            list.add(card);
+        }
+    }
 
+    public static Card getRandomSpezialkarte(int amount, Map<String, Card> originalMap) {
 
+        //
 
-    return deck; 
-    } 
+        var probabilityMap = new ArrayList<Card>();
 
+        for (var card : originalMap.values()) {
+            // Schaut ob die karte unique ist
+            if (card.unique) {
+                addMultiple(card.priority, card, probabilityMap);
+            }
+        }
 
-} */ 
+        //holt sich ein zufälliges Element aus probabilityMap und returnt es 
+        if (amount > 0) {
+            int randomValue = (int) (Math.random() * probabilityMap.size() + 1);
+            return probabilityMap.get(randomValue);
+        }
+
+        System.out.println("oops, no place for spezialkarten");
+
+        return null;
+    }
+
+}
